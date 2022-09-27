4ad3a5f0e7c50c83c15e9bcd7ba9f44184b71aa1
HubertDudaDeveloper <hubertdudadeveloper@gmail.com>
2022-09-27 18:37:07 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,23 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <main>
   <router-view/>
+</main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-
+#app {
+  height: 100vh;
+  background-color: aqua;
+}
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  height: 10vh;
+  padding: 25px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <section class="private-chat">
+      <nav class="private-chat__navbar"></nav>
+    <div class="private-chat__display">
+      <div class="private-chat__display__message">
+
+      </div>
+    </div>
+    <section class="private-chat__input">
+      <input >
+      <buttton></buttton>
+    </section>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 })
 </script>
+<style lang="scss">
+.private-chat {
+  display: grid;
+  grid-template-rows: 0.5fr 7fr 0.5fr;
+  background-color: white;
+  min-height: 90vh;
+  border-radius: 35px;
+  &__navbar {
+    border: 2px orange solid;
+  }
+  &__display {
+    border: 2px burlywood solid;
+    &__message {
+      border: 2px red solid;
+    }
+  }
+  &__input {
+    border: 2px pink solid;
+  }
+}
+</style>
